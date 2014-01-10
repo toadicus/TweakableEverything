19a256fc83ddd10f32580e0cae26fc497df93481
toadicus <drukargin@gmail.com>
2014-01-09 20:42:18 -0800

    ModuleTweakableSolarPanel.cs: Added comments and removed some extraneous fluff.

--- TweakableSolarPanels/ModuleTweakableSolarPanel.cs
+++ TweakableSolarPanels/ModuleTweakableSolarPanel.cs
@@ -13,12 +13,6 @@ namespace TweakableEverything
 {
 	public class ModuleTweakableSolarPanel : PartModule
 	{
-		protected bool firstUpdate;
-
-		protected ModuleDeployableSolarPanel panelModule;
-
-		protected Animation panelAnimation;
-
 		// Tweakable property to determine whether the solar panel should start opened or closed.
 		[KSPField(guiName = "Start", isPersistant = true, guiActiveEditor = true)]
 		[UI_Toggle(disabledText = "Retracted", enabledText = "Extended")]
@@ -31,14 +25,18 @@ namespace TweakableEverything
 		[KSPField(guiName = "Sun Tracking", isPersistant = true, guiActiveEditor = true, guiActive = true)]
 		[UI_Toggle(disabledText = "Disabled", enabledText = "Enabled")]
 		public bool sunTrackingEnabled;
+		// Save the state here so we can tell if sunTrackingEnabled has changed.
 		protected bool sunTrackingState;
 
+		// Stores the default sun tracking speed from the panel module.
 		protected float baseTrackingSpeed;
 
-		protected Transform pivotTransform;
-		protected Vector3 initialPivotVector;
-		protected Quaternion initialPivotRotation;
+		// Stores the solar panel module we're tweaking
+		protected ModuleDeployableSolarPanel panelModule;
+		// Stores the solar panel animation we're clobbering.
+		protected Animation panelAnimation;
 
+		// Gets the animationName field from the panel module.
 		protected string panelAnimationName
 		{
 			get
@@ -50,21 +48,25 @@ namespace TweakableEverything
 		// Construct ALL the objects.
 		public ModuleTweakableSolarPanel()
 		{
-			this.firstUpdate = true;
-
+			// These defaults reflect stock behavior.
 			this.StartOpened = false;
 			this.sunTrackingEnabled = true;
 		}
 
+		// Runs on PartModule startup.
 		public override void OnStart(StartState state)
 		{
+			// Startup the PartModule stuff first.
 			base.OnStart(state);
 
+			// Set our state trackers to the opposite of our states, to force first-run updates.
 			this.startOpenedState = !this.StartOpened;
 			this.sunTrackingState = !this.sunTrackingEnabled;
 
+			// Fetch the solar panel module from the part.
 			this.panelModule = base.part.Modules.OfType<ModuleDeployableSolarPanel>().FirstOrDefault();
 
+			// Yay debugging!
 			Tools.PostDebugMessage(this,
 				"panelModule: " + this.panelModule,
 				"storedAnimationTime: " + this.panelModule.storedAnimationTime,
@@ -74,34 +76,31 @@ namespace TweakableEverything
 				"originalRotation: " + this.panelModule.originalRotation
 			);
 
-			this.pivotTransform = base.part.FindModelTransform(this.panelModule.pivotName);
-			this.initialPivotVector = this.pivotTransform.localPosition;
-			this.initialPivotRotation = this.pivotTransform.localRotation;
-
-			Tools.PostDebugMessage(this,
-				"pivotTransform: " + this.pivotTransform,
-				"initial pivotVector: " + this.initialPivotVector,
-				"initial pivotRotation:" + this.initialPivotRotation
-			);
-
+			// Fetch the UnityEngine.Animation object from the solar panel module.
 			this.panelAnimation = this.panelModule.GetComponentInChildren<Animation>();
 
+			// Yay debugging!
 			Tools.PostDebugMessage(this,
 				"panelAnimation: " + this.panelAnimation,
 				"animationState: " + this.panelAnimation[this.panelModule.animationName]
 			);
 
+			// If we are in the editor and have an animation...
 			if (HighLogic.LoadedSceneIsEditor && this.panelAnimation != null)
 			{
+				// ...pre-set the panel's currentRotation...
 				this.panelModule.currentRotation = this.panelModule.originalRotation;
 
+				// ...and if our animation has an AnimationState named in the panel module...
 				if (this.panelAnimation[this.panelModule.animationName])
 				{
+					// ...Set up the AnimationState for later use.
 					this.panelAnimation.wrapMode = WrapMode.ClampForever;
 					this.panelAnimation[this.panelAnimationName].enabled = true;
 					this.panelAnimation[this.panelAnimationName].speed = 0f;
 					this.panelAnimation[this.panelAnimationName].weight = 1f;
 
+					// Yay debuggin!
 					Tools.PostDebugMessage(this,
 						"panelAnimation set wrapMode, enabled, speed, and weight."
 					);
@@ -109,8 +108,8 @@ namespace TweakableEverything
 			}
 
 			/* 
-			 * Check whether this panel is a sun tracking panel or not.  Despite its name, ModuleDeployableSolarPanel
-			 * is used for all solar panels, even those that don't deply.
+			 * Checks whether this panel is a sun tracking panel or not.  Despite its name, ModuleDeployableSolarPanel
+			 * is used for all (most?) solar panels, even those that don't deploy or rotate.
 			 * */
 			// If the panel is sun tracking panel...
 			if (this.panelModule.sunTracking)
@@ -130,33 +129,49 @@ namespace TweakableEverything
 			}
 		}
 
+		// Runs at LateUpdate.  Why?  Because.
 		public void LateUpdate()
 		{
+			// If we're in the editor...
 			if (HighLogic.LoadedSceneIsEditor)
 			{
+				// ...if StartOpened has changed and we have an Animation...
 				if (this.startOpenedState != this.StartOpened && this.panelAnimation != null)
 				{
+					// ...refresh startOpenedState
 					this.startOpenedState = this.StartOpened;
 
+					// ...and if we are starting opened...
 					if (this.StartOpened)
 					{
+						// Yay debugging!
 						Tools.PostDebugMessage(this, "Extending panel.");
 
+						// ...move the animation to the end with a "forward" play speed.
 						this.panelAnimation[this.panelAnimationName].speed = 1f;
 						this.panelAnimation[this.panelAnimationName].normalizedTime = 1f;
-						this.panelAnimation.Play(this.panelAnimationName);
+
+						// ...flag the panel as extended.
 						this.panelModule.panelState = ModuleDeployableSolarPanel.panelStates.EXTENDED;
 					}
+					// ...otherwise, we are starting closed...
 					else
 					{
+						// Yay debugging!
 						Tools.PostDebugMessage(this, "Retracting panel.");
 
+						// ...move the animation to the beginning with a "backward" play speed.
 						this.panelAnimation[this.panelAnimationName].speed = -1f;
 						this.panelAnimation[this.panelAnimationName].normalizedTime = 0f;
-						this.panelAnimation.Play(this.panelAnimationName);
+
+						// ...flag the panel as retracted.
 						this.panelModule.panelState = ModuleDeployableSolarPanel.panelStates.RETRACTED;
 					}
 
+					// ...play the animation, because it's so very pretty.
+					this.panelAnimation.Play(this.panelAnimationName);
+
+					// ...update the persistence data for the solar panel accordingly.
 					this.panelModule.storedAnimationTime = this.panelAnimation[this.panelAnimationName].normalizedTime;
 					this.panelModule.storedAnimationSpeed = this.panelAnimation[this.panelAnimationName].speed;
 					this.panelModule.stateString =
@@ -164,26 +179,22 @@ namespace TweakableEverything
 				}
 			}
 
-			if (HighLogic.LoadedSceneIsFlight)
-			{
-				Tools.PostDebugMessage(this,
-					"storedAnimationTime: " + this.panelModule.storedAnimationTime,
-					"storedAnimationSpeed: " + this.panelModule.storedAnimationSpeed,
-					"stateString: " + this.panelModule.stateString,
-					"currentRotation: " + this.panelModule.currentRotation
-				);
-			}
-
+			// If this panel is tracking-enabled and our sun tracking state has changed...
 			if (this.panelModule.sunTracking && this.sunTrackingEnabled != this.sunTrackingState)
 			{
+				// ..refresh our sunTrackingState
 				this.sunTrackingState = this.sunTrackingEnabled;
 
+				// ...and if we're tracking the sun...
 				if (this.sunTrackingEnabled)
 				{
+					// ...ensure the panel's tracking speed is set per it's original value
 					this.panelModule.trackingSpeed = this.baseTrackingSpeed;
 				}
+				// ...otherwise, we're not tracking the sun...
 				else
 				{
+					// ...so set the panel's tracking speed to zero
 					this.panelModule.trackingSpeed = 0;
 				}
 			}
