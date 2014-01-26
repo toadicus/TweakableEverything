d4f8a011091fc2f8b3881000682f3f0d2477e4a2
toadicus <drukargin@gmail.com>
2014-01-26 08:57:50 -0800

    Added AssemblyInfo files for manual version tracking because I hate myself or something.

---
+++ TweakableLadders/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿// TweakableLadders © 2014 toadicus
+//
+// This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a
+// copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+[assembly: AssemblyTitle("TweakableLadders")]
+[assembly: AssemblyDescription("Adds useful 'tweakable' configuration options for retractable ladders.")]
+[assembly: AssemblyCopyright("toadicus")]
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+[assembly: AssemblyVersion("0.6.0.*")]
+// The following attributes are used to specify the signing key for the assembly,
+// if desired. See the Mono documentation for more information about signing.
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
