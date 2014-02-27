cc2f7ec19fb4744cdbb14809a92c0c3a10a02668
Andy <andy@wtribe.com>
2014-02-26 17:14:19 -0800

    Version 0.6.3.

--- TweakableAnimateGeneric/Properties/AssemblyInfo.cs
+++ TweakableAnimateGeneric/Properties/AssemblyInfo.cs
@@ -1,20 +1,20 @@
-﻿using System.Reflection;
+﻿// TweakableAnimateGeneric © 2014 toadicus
+//
+// This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License. To view a
+// copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/3.0/
+
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 [assembly: AssemblyTitle("TweakableAnimateGeneric")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("andy")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("Adds useful 'tweakable' configuration options for generic animation modules.")]
+[assembly: AssemblyCopyright("toadicus")]
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.6.3.*")]
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
 //[assembly: AssemblyDelaySign(false)]
