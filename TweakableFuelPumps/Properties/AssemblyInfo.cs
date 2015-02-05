83a58f7722f17c6b9291d32c4efc93d50b6aca40
toadicus <drukargin@gmail.com>
2015-02-05 10:42:24 -0800

    Version 1.8

--- TweakableFuelPumps/Properties/AssemblyInfo.cs
+++ TweakableFuelPumps/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
-﻿// TweakableRCS, a TweakableEverything module
+﻿// TweakableFuelPumps, a TweakableEverything module
 //
 // AssemblyInfo.cs
 //
-// Copyright © 2014, toadicus
+// Copyright © 2015, toadicus
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without modification,
@@ -25,21 +25,18 @@
 // SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
 // WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
-[assembly: AssemblyTitle("TweakableRCS")]
-[assembly: AssemblyDescription("Adds useful 'tweakable' configuration options for RCS thrusters.")]
+
+[assembly: AssemblyTitle("TweakableFuelPumps")]
+[assembly: AssemblyDescription("KSP Mod that adds action group items to fuel tanks.")]
 [assembly: AssemblyCopyright("toadicus")]
+
 // The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
-[assembly: AssemblyVersion("1.8.*")]
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
 
+[assembly: AssemblyVersion("1.8.*")]
