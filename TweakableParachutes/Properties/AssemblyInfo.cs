77eba216c65cdc00526c3b8df9c44dcb93acad4f
toadicus <drukargin@gmail.com>
2016-04-20 21:20:45 -0700

    Version 1.15

--- TweakableParachutes/Properties/AssemblyInfo.cs
+++ TweakableParachutes/Properties/AssemblyInfo.cs
@@ -39,4 +39,4 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.14.0.*")]
+[assembly: AssemblyVersion("1.15.0.*")]
